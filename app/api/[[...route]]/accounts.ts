ad9fc8fe0db2328eaad8a037101138b8d2ca2d8a
Vinicius Raniery <viniciusraniery@outlook.com>
2024-09-28 13:09:01 -0300

    commit

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -16,7 +16,7 @@ const app = new Hono()
         const auth = getAuth(c);
 
         if (!auth?.userId) {
-          return c.json({ error: "Unauthorized" }, 401);
+          return c.json({ error: "Não autorizado" }, 401);
         }
 
     const data = await db
@@ -29,6 +29,44 @@ const app = new Hono()
 
     return c.json({ data });
 })
+.get(
+    "/:id",
+    zValidator("param", z.object({
+        id: z.string().optional(),
+    })),
+    clerkMiddleware(),
+    async (c) => {
+        const auth = getAuth(c);
+        const { id } = c.req.valid("param");
+
+        if (!id) {
+            return c.json({ error: "ID ausente" }, 400);
+        }
+
+        if (!auth?.userId) {
+            return c.json({ error: "Não autorizado" }, 401);
+        }
+
+        const [data] = await db
+        .select({
+            id: accounts.id,
+            name: accounts.name,
+        })
+        .from(accounts)
+        .where(
+            and(
+                eq(accounts.userId, auth.userId),
+                eq(accounts.id, id)
+            ),
+        );
+
+        if (!data) {
+            return c.json({ error: "Não encontrado" }, 404);
+        }
+
+        return c.json({ data });
+    }
+    )
 .post(
     "/",
     clerkMiddleware(),
@@ -40,7 +78,7 @@ const app = new Hono()
         const values = c.req.valid("json");
 
         if (!auth?.userId) {
-            return c.json({ error: "Unauthorized" }, 401);
+            return c.json({ error: "Não autorizado" }, 401);
         }
 
         const [data] = await db.insert(accounts).values({
@@ -65,7 +103,7 @@ const app = new Hono()
         const values = c.req.valid("json");
 
         if (!auth?.userId) {
-            return c.json({ error: "Unauthorized" }, 401);
+            return c.json({ error: "Não autorizado" }, 401);
         }
 
         const data = await db
@@ -79,6 +117,92 @@ const app = new Hono()
         .returning({
             id: accounts.id,
         });
+
+        return c.json({ data });
+    },
+)
+.patch(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+        "param",
+        z.object({
+            id: z.string().optional(),
+        }),
+    ),
+    zValidator(
+        "json",
+        insertAccountSchema.pick({
+            name: true,
+        })
+    ),
+    async (c) => {
+        const auth = getAuth(c);
+        const { id } = c.req.valid("param");
+        const values = c.req.valid("json");
+
+        if (!id) {
+            return c.json({ error: "ID ausente" }, 400);
+        }
+
+        if (!auth?.userId) {
+            return c.json({ error: "Não autorizado" }, 401);
+        }
+
+        const [data] = await db
+        .update(accounts)
+        .set(values)
+        .where(
+            and(
+                eq(accounts.userId, auth.userId),
+                eq(accounts.id, id),
+            ),
+        )
+        .returning();
+
+        if (!data) {
+            return c.json({ error: "Não encontrado" }, 404);
+        }
+
+        return c.json({ data });
+    }
+)
+.delete(
+    "/:id",
+    clerkMiddleware(),
+    zValidator(
+        "param",
+        z.object({
+            id: z.string().optional(),
+        }),
+    ),
+    async (c) => {
+        const auth = getAuth(c);
+        const { id } = c.req.valid("param");
+
+        if (!id) {
+            return c.json({ error: "ID ausente" }, 400);
+        }
+
+        if (!auth?.userId) {
+            return c.json({ error: "Não autorizado" }, 401);
+        }
+
+        const [data] = await db
+        .delete(accounts)
+        .where(
+            and(
+                eq(accounts.userId, auth.userId),
+                eq(accounts.id, id)
+            ),
+        )
+        .returning({
+            id: accounts.id,
+        });
+
+        if (!data) {
+            return c.json({ error: "Não encontrado" }, 404);
+        }
 
         return c.json({ data });
     },
