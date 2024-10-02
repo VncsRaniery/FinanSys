b124a7e738134d936a0dca48c2247e99db1c174d
Vinicius Raniery <viniciusraniery@outlook.com>
2024-10-02 14:29:15 -0300

    Adicionando filtros de conta e de data

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { HeaderLogo } from "./HeaderLogo";
 import Navigation from "./Navigation";
 import { Loader2 } from "lucide-react";
 import { WelcomeMsg } from "./WelcomeMsg";
+import { Filters } from "./Filters";
 
 export const Header = () => {
   return (
@@ -21,6 +22,7 @@ export const Header = () => {
           </ClerkLoading>
         </div>
         <WelcomeMsg />
+        <Filters />
       </div>
     </header>
   );
