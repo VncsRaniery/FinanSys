6e85a0076a354317e2c567e3e7305470d5a2d46e
Vinicius Raniery <152623369+VncsRaniery@users.noreply.github.com>
2025-01-07 20:09:30 -0300

    Commit

--- src/components/GraficoPizza.tsx
+++ src/components/GraficoPizza.tsx
@@ -29,12 +29,6 @@ const CORES = [
   "hsl(var(--chart-4))",
   "hsl(var(--chart-5))",
   "hsl(var(--chart-6))",
-  "hsl(var(--chart-7))",
-  "hsl(var(--chart-8))",
-  "hsl(var(--chart-9))",
-  "hsl(var(--chart-10))",
-  "hsl(var(--chart-11))",
-  "hsl(var(--chart-12))",
 ];
 
 type Props = {
